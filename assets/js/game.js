ad723cfe1ea5fbe84557e3aefd00deec448586c0
Lindsey <scarrlettearianne@gmail.com>
2021-03-28 01:07:21 -0700

    Changed Shop Values to Numeric

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,10 +34,10 @@ var fight = function (enemy) {
     // repeat and execute as long as the enemy-robot is alive 
     while (playerInfo.health > 0 && enemy.health > 0) {
       // ask player if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
-    }
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
       // if yes (true), leave fight
       if (confirmSkip) {
@@ -148,28 +148,22 @@ var endGame = function () {
 var shop = function () {
   // ask the player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
-  // use switch to carry out action
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+  // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case "refill":
-    case "REFILL":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "upgrade":
-    case "UPGRADE":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "leave":
-    case "LEAVE":
+    case 3:
       window.alert("Leaving the store.");
-
-      // do nothing, so function will end
       break;
     default:
       window.alert("You did not pick a valid option. Try again.");
-
-      // call shop() again to force player to pick a valid option
       shop();
       break;
   }
